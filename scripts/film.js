6fd0483333968ab7dee572d17c330d97d16813a0
Denis Tsvikevich <dtsvikevich@mail.ru>
2020-10-21 01:56:09 +0300

    adaptate

---
+++ scripts/film.js
@@ -0,0 +1,47 @@
+const playerPlaceholder = document.querySelector("#playerPlaceholder");
+const playerFrame = document.querySelector("#playerFrame");
+
+playerFrame.onload = function () { 
+    playerFrame.classList.toggle("hide");
+    playerPlaceholder.classList.toggle("hide");
+};
+
+function switchPlayer(src) {
+    playerFrame.classList.toggle("hide");
+    playerPlaceholder.classList.toggle("hide");
+
+    playerFrame.src = "https://www.youtube.com/embed/" + src;
+}
+
+let activeTranslation = document.getElementById("translation-default");
+activeTranslation.classList.add("active");
+switchPlayer(activeTranslation.getAttribute("data-src"));
+
+function translationItemClick(event) {
+    let src = event.target.getAttribute("data-src");
+
+    switchPlayer(src);
+
+    activeTranslation.classList.remove("active");
+    event.target.classList.add("active");
+    activeTranslation = event.target;
+}
+
+let translationList = document.querySelector("#translation-list");
+
+for (const translationItem of translationList.children) {
+    translationItem.addEventListener("click", translationItemClick);
+}
+
+let prevText = "Скрыть переводы";
+
+let toggleTranslationListButton = document.getElementById("toggle-translation-list");
+let toggleTranslationListButtonText = toggleTranslationListButton.children[0];
+
+toggleTranslationListButton.addEventListener("click", () => {
+    translationList.classList.toggle("hide");
+    toggleTranslationListButton.classList.toggle("opened");
+    let newPrevText = toggleTranslationListButtonText.innerText;
+    toggleTranslationListButtonText.innerText = prevText;
+    prevText = newPrevText;
+});
